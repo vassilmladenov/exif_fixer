192e0ecc3af25a49a1be645e853396fe47aed66d
Vassil Mladenov <vmladenov@icloud.com>
2016-01-04 22:05:42 -0600

    sucessfully fixed exif data

--- exif_fixer.py
+++ exif_fixer.py
@@ -1,2 +1,23 @@
 import time
-# time.strptime("06-30-08_2108","%m-%d-%y_%H%M")
+import sys, os
+
+filepath = os.path.abspath(sys.argv[1])
+
+# magic numbers, need to change this if jpg is jpeg
+name = os.path.basename(filepath)[:-4] # strip off extension 
+ext = os.path.basename(filepath)[-3:] 
+correct_time = time.strptime(name ,"%m-%d-%y_%H%M")
+
+if ext.lower() == "jpg":
+    argument = "datetimeoriginal"
+elif ext.lower() == "3gp":
+    argument = "createdate"
+else:
+    sys.exit(1)
+
+formatted_time = time.strftime("%Y:%m:%d %H:%M:%S", correct_time)
+
+command = "exiftool '-" + argument + "=" + formatted_time + "' '" + filepath + "'"
+
+print command
+os.system(command)
